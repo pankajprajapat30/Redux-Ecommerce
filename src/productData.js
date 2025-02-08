4db24b32d60d366329176511ab58f24c9cab5901
pankaj prajapat <pp3035155@gmail.com>
2025-02-08 16:30:05 +0530

    fixed image path

--- src/productData.js
+++ src/productData.js
@@ -3,56 +3,56 @@ export const productData = [
         id: 1,
         title: "samsung galaxy s7",
         price: 1000.0,
-        img: "/public/samsung.jpg",
+        img: "/samsung.jpg",
         quantity: 1
     },
     {
         id: 2,
         title: "moto gs plus",
         price: 6000.0,
-        img: "/public/moto.jpg",
+        img: "/moto.jpg",
         quantity: 1
     },
     {
         id: 3,
         title: " xiaomi Redmi Note 2 ",
         price: 5000.0,
-        img: "/public/xiaomi.jpg",
+        img: "/xiaomi.jpg",
         quantity: 1
     },
     {
         id: 4,
         title: "Nokia G20",
         price: 7000.0,
-        img: "/public/nokia.jpg",
+        img: "/nokia.jpg",
         quantity: 1
     },
     {
         id: 5,
         title: "Apple i phone",
         price: 4000.0,
-        img: "/public/apple.jpg",
+        img: "/apple.jpg",
         quantity: 1
     },
     {
         id: 6,
         title: "vivo",
         price: 3000.0,
-        img: "/public/vivo.webp",
+        img: "/vivo.webp",
         quantity: 1
     },
     {
         id: 7,
         title: " one+",
         price: 8000.0,
-        img: "/public/oneplus.webp",
+        img: "/oneplus.webp",
         quantity: 1
     },
     {
         id: 8,
         title: " nothing",
         price: 2000.0,
-        img: "/public/realme.jpg",
+        img: "/realme.jpg",
         quantity: 1
     }
 
